bbb956fe8492c6324793b2bcc66fbd0c51034c79
Franco Silva <franco_tigres18@hotmail.com>
2023-06-03 11:00:12 -0600

    Importacion componente fixed

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, reactive, onMounted, watch } from "vue";
 import { db } from "./data/guitarras";
-import Guitarra from "./components/Guitarra.Vue";
+import Guitarra from "./components/Guitarra.vue";
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
